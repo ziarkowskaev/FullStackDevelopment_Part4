5ab9aa9be7847fa4f2ea51ec2f2a03b731a71850
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-05-28 22:24:05 +0300

    Commit 4.1 and 4.2

---
+++ controllers/blogs.js
@@ -0,0 +1,24 @@
+const Blog = require('../models/blog')
+const blogsRouter = require('express').Router()
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+    .catch(error => next(error))
+})
+
+module.exports = blogsRouter
