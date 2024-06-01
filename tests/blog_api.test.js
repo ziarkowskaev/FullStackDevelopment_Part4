a8a1115f4e306e6f110f677dc7e9557eeae77a6a
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-06-01 22:03:48 +0300

    Adding tests for 4.13-4.14
    Additionally refactoring tests with "describe".

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach} = require('node:test')
+const { test, after, beforeEach, describe} = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -9,25 +9,35 @@ const helper = require('./test_helper')
 
 const Blog = require('../models/blog')
 
-beforeEach(async () => {
+
+
+describe("retriving the blogs from the database", () => {
+  beforeEach(async () => {
     await Blog.deleteMany({})
     await Blog.insertMany(helper.initialBlogs)
 })
 
-test('there are two blogs', async () => {
-const response = await api.get('/api/blogs')
-
-assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+    })
+    
+  test('the unique identifier property of the blog posts is named id', async () => {
+      const response =  await api.get('/api/blogs')
+                
+      const ids = response.body.map(e => e.id)
+      assert(ids.length > 0)
+  })
 })
 
-test('the unique identifier property of the blog posts is named id', async () => {
-   const response =  await api.get('/api/blogs')
-             
-   const ids = response.body.map(e => e.id)
-   assert(ids.length > 0)
+describe('adding the blogs', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
 })
 
-test('a blog can be added ', async () => {
+  test('a blog can be added ', async () => {
     const newBlog = {
         title: "Canonical string reduction",
         author: "Edsger W. Dijkstra",
@@ -106,6 +116,60 @@ test('a blog can be added ', async () => {
   
     assert.strictEqual(response.body.length, helper.initialBlogs.length)
   })
+
+})
+
+describe('deletion of a blog', () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+    const urls = blogsAtEnd.map(r => r.url)
+    assert(!urls.includes(blogToDelete.url))
+  })
+})
+
+
+describe('updating the blog information'), () => {
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+})
+
+  test('succeeds with status code 204 if id is valid', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToUpdate = blogsAtStart[0]
+
+    const updateWith = {
+      likes: 123
+    }
+
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updateWith)
+      .expect(204)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    assert.deepstrictEqual(blogsAtEnd[0], helper.initialBlogs[0])
+
+  })
+
+}
+
+
 
 
 after(async () => {
