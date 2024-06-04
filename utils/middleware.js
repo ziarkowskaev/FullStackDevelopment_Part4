fa224f7668b942484c98443a6c39afeb4014e06c
Ewelina Ziarkowska <124273313+ziarkowskaev@users.noreply.github.com>
2024-06-04 17:17:40 +0300

    Adding Part4.20

--- utils/middleware.js
+++ utils/middleware.js
@@ -12,6 +12,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const tokenExtractor = async (request,response,next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  }else{
+    request.token = null
+  }
+  next();
+}
+
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
@@ -37,5 +47,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
